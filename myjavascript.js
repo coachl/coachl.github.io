d3469f828fd1514aaaed27e8766a462d3577258c
coachl <lakishagreenwade@Lakishas-MacBook.local>
2016-01-31 15:09:36 -0500

    finalfile
    last work from coding class on Jan 31, 2016

---
+++ myjavascript.js
@@ -0,0 +1,10 @@
+var (temperature = 25;
+if (temperature < 50) {
+	console.log("Wear a coat.");
+}
+else if (temperature <0) {
+	console.log("Stay inside");
+}
+else{
+	console.log("Wear whatever you want.");
+};
